4737475bcf22afca96099a8110d5c86faa02a73a
aaronBurns59 <aaronburns59@gmail.com>
2019-03-25 19:17:12 +0000

    added the code that deals with the brackets of infix strings

--- project.py
+++ project.py
@@ -11,22 +11,32 @@
 
 def shunt(infix): 
     #stores operators as they are read in in order of precedence given by the dictionary
+    #A string can be used as a stack as it is only dealing with character
     operatorStack = ''
     #stores all characters in the proper postfix notation 
     postfix = '' 
     #A dictionary that gives all the symbols into an order of precedence 
-    specialSymbols('*':50, '+':40, '.':30, '|':20)
+    specialSymbols ={'*':50, '+':40, '.':30, '|':20}
 
     #for loop that continues for the lenght of the string input
     for i in infix:
+        #Postfix does not use brackets so they need to be explicitly dealt with
         if i  == '(':
-        
-        elif i == ')'
-
+            #appends the '(' to the stack
+            stack = stack + i
+        elif i == ')':
+            #checks to see if the last item on the stack is a '('
+            while stack[-1] != '(':
+                #adds the last symbol to the postfix string i.e. the symbol before the ')'
+                postfix = postfix + specialSymbols[-1]
+                 #sets the stack equal to the second last charatcer of the String
+                specialSymbols = specialSymbols[:-1]
+            #This line deletes the '(' from the stack
+            specialSymbols = specialSymbols[:-1]
         elif i in specialSymbols:
 
         #else handles all normal characters 1,0 a-z, A-Z 
-        else
+        else:
 
     #finished
     return postfix
