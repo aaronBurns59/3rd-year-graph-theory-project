a289a815f9c0b7810b43cd4edf70f6a0ece72ba1
aaronBurns59 <aaronburns59@gmail.com>
2019-03-25 19:35:36 +0000

    finished the shunting yard function aswell as fixed minor code error

--- project.py
+++ project.py
@@ -23,22 +23,35 @@ def shunt(infix):
         #Postfix does not use brackets so they need to be explicitly dealt with
         if i  == '(':
             #appends the '(' to the stack
-            stack = stack + i
+            operatorStack = operatorStack + i
         elif i == ')':
             #checks to see if the last item on the stack is a '('
-            while stack[-1] != '(':
+            while operatorStack[-1] != '(':
                 #adds the last symbol to the postfix string i.e. the symbol before the ')'
-                postfix = postfix + specialSymbols[-1]
+                postfix = postfix + operatorStack[-1]
                  #sets the stack equal to the second last charatcer of the String
-                specialSymbols = specialSymbols[:-1]
+                operatorStack = operatorStack[:-1]
             #This line deletes the '(' from the stack
-            specialSymbols = specialSymbols[:-1]
+            operatorStack = operatorStack[:-1]
+        #handles any of the symbols that appear in the dictionary above
         elif i in specialSymbols:
-
+            #I DONT FULLY KNOW THIS YET
+            while operatorStack and specialSymbols.get(i, 0) <= specialSymbols.get(operatorStack[-1],0):
+                postfix= postfix + operatorStack[-1]
+                operatorStack = operatorStack[:-1]
+            #adds new specials symbols read in onto the operator stack
+            operatorStack = operatorStack + i    
         #else handles all normal characters 1,0 a-z, A-Z 
         else:
-
-    #finished
+            #Adds whatever character is read by the for loop to the postfix String 
+            postfix = postfix + i
+    #The while loop is used to empty the operator stack into the postfix string
+    while operatorStack:
+        #adds the last symbol on the operator stack to the postfix string
+        postfix = postfix + operatorStack[-1]
+        #sets the operator stack equal to the second last item
+        operatorStack= operatorStack[:-1]
     return postfix
 
+print(shunt('A*B+C'))
 
