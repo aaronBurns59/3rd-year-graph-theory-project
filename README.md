cf8023e5b120639768338c801778c356a35c522e
aaronBurns59 <aaronburns59@gmail.com>
2019-03-26 19:20:04 +0000

    Created two new classes one for states and one for NFA's and updated the readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Infix to Postfix (Shunting Yard Algorithm)
+==========================================
+
+#for this portion of the program I used strings in place of stacks because all the shunt function details with are characters
 
 The first part of the project is to convert any regular expressions that may be entered by the user into postfix notation.
 The reason for doing this is, computers have an easier time reading them than infix notation as they can process it in one left to right read. They do not have to go back over the string.
@@ -10,4 +13,15 @@ The for loop continues for the lenght of the string that is passed into the func
 The if statement is entered if an opening bracket is encountered on the stack. The bracket is popped to the stack and that is all that is needed to be done so the loop iterates to the next item on the stack
 
 The first elif is entered when the item on the stack is a closing bracket. Inside this statment a while loop checks to see if the the last item on the stack is not an opening bracket if not than whatever is on the operator stack is added to the postfix string and the stack is set equal to its second last element(essentially popping it). When all the items between the opening and closing bracket have been put into the postfix string. The opening bracket is removed from the stack and discarded
- 
+
+The second Elif handles any symbols that go into the operator stack that are also declared inside the dictionary. It checks using a while loop to see if the operator is in the dictionary and what precedence it has in the dictionary (* has a higher precedenc then | and .). The precedence of the operator determines if it is added to the postfix string before or after the symbol currently on the operatorStack. Some symbols have equal precedence like - and +, in that case which ever is on the operatorStack will be pushed first due to this programs lef to right association.Once the operators are added to the postfix string the operatorStack is set equal to its second last character.
+
+The else deals with any characters that are not brackets or operators. Symbols that are just regular characters like a,b, 1 or 0. These characters aree just added to the postfix string as they enter the for loop
+
+ At the end of the shunt function there is a while loop that empties the operatorStack in case there are any symbols left on it and it returns the postfix string that was converted from infix.
+
+
+ Creating NFA's from the postfix RE (Thompson's Construction)
+=============================================================
+
+Two classes are used to represent the NFA's and the states that make up those NFA's.  The NFA class has a constructor io initialise its properties.  The state class has three variables one for the label of the state and two for the edges. The edges will be used later on the connect the states in the NFA
--- project.py
+++ project.py
@@ -1,60 +1,85 @@
-#Aaron Burns
-#Program Basic Description:
-#A program which converts infix to postfix
-#Builds NFA's from regular expressions
-#Uses the single NFA's for each character to build larger NFA's
-#And use that NFA to recognise any string of text
+# Aaron Burns
+# Program Basic Description:
+# A program which converts infix to postfix
+# Builds NFA's from regular expressions
+# Uses the single NFA's for each character to build larger NFA's
+# And use that NFA to recognise any string of text
 
-#Infix => Postfix Notation 
-#The reason for doing this is that Computers have an easier time reading Postifx notations of strings
-#because they can do it in one left to right read
+# Infix => Postfix Notation 
+# The reason for doing this is that Computers have an easier time reading Postifx notations of strings
+# because they can do it in one left to right read
 
-def shunt(infix): 
-    #stores operators as they are read in in order of precedence given by the dictionary
-    #A string can be used as a stack as it is only dealing with character
+def shunt(infix):
+    """Converts infix regular expressions postfix notation"""
+    # stores operators as they are read in in order of precedence given by the dictionary
+    # A string can be used as a stack as it is only dealing with character
     operatorStack = ''
-    #stores all characters in the proper postfix notation 
+    # stores all characters in the proper postfix notation 
     postfix = '' 
-    #A dictionary that gives all the symbols into an order of precedence
-    #Set up as left to right association meaning if two symbols of the same precedence meet on the stack
-    #the symbol currently on the stack is popped first   
+    # A dictionary that gives all the symbols into an order of precedence
+    # Set up as left to right association meaning if two symbols of the same precedence meet on the stack
+    # the symbol currently on the stack is popped first   
     specialSymbols ={'*':50, '+':40, '-':40, '.':30, '|':20}
 
-    #for loop that continues for the lenght of the string input
+    # for loop that continues for the lenght of the string input
     for i in infix:
-        #Postfix does not use brackets so they need to be explicitly dealt with
+        # Postfix does not use brackets so they need to be explicitly dealt with
         if i  == '(':
-            #appends the '(' to the stack
+            # appends the '(' to the stack
             operatorStack = operatorStack + i
         elif i == ')':
-            #checks to see if the last item on the stack is a '('
+            # checks to see if the last item on the stack is a '('
             while operatorStack[-1] != '(':
-                #adds the last symbol to the postfix string i.e. the symbol before the ')'
-                #sets the stack equal to the second last charatcer of the String
+                # adds the last symbol to the postfix string i.e. the symbol before the ')'
+                # sets the stack equal to the second last charatcer of the String
                 postfix, operatorStack = postfix + operatorStack[-1],  operatorStack[:-1]
-            #This line deletes the '(' from the stack
+            # This line deletes the '(' from the stack
             operatorStack = operatorStack[:-1]
-        #handles any of the symbols that appear in the dictionary above
+        # handles any of the symbols that appear in the dictionary above
         elif i in specialSymbols:
-            ##NEED COMMENT HERE 
+            # checks if the current symbol is in the dictionary and checks the precedence of the operators in question
+            # the 0 in the get is a default of the .get function
             while operatorStack and specialSymbols.get(i, 0) <= specialSymbols.get(operatorStack[-1],0):
-                #adds new specials symbols read in onto the operator stack
+                # adds new specials symbols read in onto the operator stack
                 postfix, operatorStack = postfix + operatorStack[-1], operatorStack[:-1]
             operatorStack = operatorStack + i    
-        #else handles all normal characters 1,0 a-z, A-Z 
+        # else handles all normal characters 1,0 a-z, A-Z 
         else:
-            #Adds whatever character is read by the for loop to the postfix String 
+            # Adds whatever character is read by the for loop to the postfix String 
             postfix = postfix + i
-    #The while loop is used to empty the operator stack into the postfix string
+    # The while loop is used to empty the operator stack into the postfix string
     while operatorStack:
-        #adds the last symbol on the operator stack to the postfix string
+        # adds the last symbol on the operator stack to the postfix string
         postfix, operatorStack = postfix + operatorStack[-1], operatorStack[:-1]
-        #sets the operator stack equal to the second last item
+        # sets the operator stack equal to the second last item
     return postfix
 
-#testing that the algorithm works correctly
+# testing that the algorithm works correctly
 print(shunt('A*B+C'))
 print(shunt('A+B*C'))
 print(shunt('A*(B+C)'))
-
 print(shunt('A-B+C'))
+
+# This class is used to repsent the NFA's that will be used in this program
+# NFA's are made up of states
+class NFA:
+    initial = None
+    final = None
+
+# this class used to represent a state in an NFA that is used in Thompson's construction
+# States in Thompson's Construction can't have more than 2 edges 
+# the label just repesent the symbol that is entered e.g 1,0,a,b
+class state:
+    #The value None implies that you don't set the value yet
+    label = None
+    edge1 = None
+    edge2 = None
+    
+    #C onstructor for the NFA class
+    # self references the current instance of the class
+    # self has to be called first and is not needed to create an instance of the class
+    def __init__(self, initial, accept):
+        self.initial=initial
+        self.final=final
+
+
